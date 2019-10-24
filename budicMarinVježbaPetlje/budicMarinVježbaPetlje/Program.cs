3b1f68671e8dac05c60c8e485d53ad554ccb6bad
budicmarin <55134480+budicmarin@users.noreply.github.com>
2019-10-24 11:31:38 +0200

    Add files via upload

---
+++ budicMarinVježbaPetlje/budicMarinVježbaPetlje/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinVježbaPetlje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Upisi text: ");
+            string unosTeksta = Console.ReadLine();
+            
+            for (int i = unosTeksta.Length; i > 0; i--)
+            {
+                Console.WriteLine(unosTeksta.Substring(0,i));
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
